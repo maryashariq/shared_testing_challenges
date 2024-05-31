e2c746c23545745ed8f505120256bdee816a73da
Marya <ms930652210@googlemail.com>
2024-05-31 11:43:52 +0100

    these are the code files for the shared test challenges we did together

---
+++ lib/most_often.py
@@ -0,0 +1,40 @@
+# File: lib/most_often.py
+
+class MostOften:
+    def __init__(self, starting_list):
+        self.starting_list = starting_list
+
+    def add_new(self, new_item):
+        self.starting_list.append(new_item)
+
+    def get_most_often(self):
+        # use set() to create a list of unique items
+        unique_items = set(self.starting_list)
+        print(unique_items)
+
+        # set up our highest recorded variables
+        highest_count = 0
+        highest_items = []
+
+        for item in unique_items:
+            # count the number of instances of that item in the list
+            count = self.starting_list.count(item)
+            # if we've found a new highest count...
+            if count > highest_count:
+                highest_count = count
+                highest_items = [item]
+                # print(highest_items)
+            # but if we've found an equally high count...
+            elif count == highest_count:
+                highest_items.append(item)
+                # print(highest_items)
+
+        # if we have a clear winner, return it
+        if len(highest_items) == 1:
+            return highest_items[0]
+        # otherwise we'll say there's "no clear winner"
+        else:
+            return "no clear winner"
+
+# most_often_object = MostOften(['a', 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c'])
+# print(most_often_object.get_most_often())
